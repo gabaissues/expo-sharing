a68e5bd2b6292318ddf0546de766970cef976692
gbxxy <ogabrielskj@gmail.com>
2021-09-12 17:12:57 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, TouchableOpacity, View } from 'react-native';
+
+import * as ImagePicker from 'expo-image-picker'
+import * as Sharing from 'expo-sharing'
 
 export default function App() {
+  const [image, setImage] = React.useState('https://picsum.photos/200/300')
+
+  const handleShowImagePicker = async(): Promise<void> => {
+
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if(status != 'granted') {
+
+      alert('Precisamos da permissão para continuar')
+
+    } else {
+
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: false,
+        aspect: [4, 3],
+        base64: false,
+        quality: 1
+      })
+
+      if(result.cancelled) return;
+      setImage(result.uri)
+
+      let sharing = await Sharing.isAvailableAsync()
+      if(!sharing) return alert('Seu celular não suporta o recurso de compartilhamento.')
+
+      await Sharing.shareAsync(result.uri)
+
+    }
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.img} source={{ uri: image }} />
+      <TouchableOpacity onPress={() => handleShowImagePicker()}><Text>Clique em mim</Text></TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +53,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img: {
+    width: 200,
+    height: 300
+  }
 });
